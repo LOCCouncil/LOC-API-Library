38fd0f3577256c99fa9453a17b9c345197b41be4
Matthew R <40540813+mattsilver1@users.noreply.github.com>
2018-12-04 19:21:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
-  getApplicationData: require('./src/client/getApplicationData'),
-  putApplicationPrefix: require('./src/client/putApplicationPrefix'),
-  getMemberData: require('./src/member/getMemberData'),
-  postMemberNick: require('./src/member/postMemberNick'),
-  putMemberSelfrole: require('./src/member/putMemberSelfrole')
+  getApplicationData: require('./src/client/getApplicationData')
+  //putApplicationPrefix: require('./src/client/putApplicationPrefix'),
+  //getMemberData: require('./src/member/getMemberData'),
+  //postMemberNick: require('./src/member/postMemberNick'),
+  //putMemberSelfrole: require('./src/member/putMemberSelfrole')
 };
